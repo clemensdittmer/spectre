514165d5518b473d59385f7eaf6bd4852324298f
Clemens Dittmer <clemens_dittmer@hotmail.com>
2025-06-16 15:34:53 +0200

    Amended Zeroth Order Puncture Files cause they also compute the connection terms with spin now.

--- src/Evolution/Systems/CurvedScalarWave/Worldtube/InitialData/ZerothOrderPuncture.cpp
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/InitialData/ZerothOrderPuncture.cpp
@@ -22,10 +22,13 @@ namespace CurvedScalarWave::AnalyticData {
 ZerothOrderPuncture::ZerothOrderPuncture(
     const std::array<double, 3> particle_position,
     const std::array<double, 3> particle_velocity, const double particle_charge,
+    const std::array<double, 3> bh_spin,
     const Options::Context& /*context*/)
     : particle_position_(particle_position),
       particle_velocity_(particle_velocity),
-      particle_charge_(particle_charge) {
+      particle_charge_(particle_charge),
+      bh_spin_(bh_spin),
+      kerr_schild_({1., bh_dimensionless_spin, {{0., 0., 0.}}}) {
   const auto background_vars = kerr_schild_.variables(
       particle_position_, 0.,
       tmpl::list<gr::Tags::SpacetimeChristoffelSecondKind<double, 3,
@@ -60,7 +63,7 @@ ZerothOrderPuncture::variables(const tnsr::I<DataVector, 3>& x,
       puncture(get<0>(centered_coords).size());
   CurvedScalarWave::Worldtube::puncture_field_0(
       make_not_null(&puncture), centered_coords, particle_position_,
-      particle_velocity_, geodesic_acceleration_, 1.);
+      particle_velocity_, geodesic_acceleration_, 1., bh_spin_);
   puncture *= particle_charge_;
   const auto background_vars =
       kerr_schild_.variables(x, 0.,
@@ -89,6 +92,7 @@ void ZerothOrderPuncture::pup(PUP::er& p) {
   p | particle_velocity_;
   p | geodesic_acceleration_;
   p | particle_charge_;
+  p | bh_spin_;
   p | kerr_schild_;
 }
 
@@ -101,7 +105,8 @@ bool operator==(const ZerothOrderPuncture& lhs,
          lhs.particle_velocity_ == rhs.particle_velocity_ and
          lhs.geodesic_acceleration_ == rhs.geodesic_acceleration_ and
          lhs.particle_charge_ == rhs.particle_charge_ and
-         lhs.kerr_schild_ == rhs.kerr_schild_;
+         lhs.kerr_schild_ == rhs.kerr_schild_ and
+         lhs.bh_spin_ == rhs.bh_spin_;
 }
 
 bool operator!=(const ZerothOrderPuncture& lhs,
