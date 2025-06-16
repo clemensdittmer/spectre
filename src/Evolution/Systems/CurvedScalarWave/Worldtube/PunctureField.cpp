9b70e8492933cff1ed92ed9fe42829a2c58782b1
Clemens Dittmer <clemens_dittmer@hotmail.com>
2025-06-16 21:02:10 +0200

    Added the Kerr File into the scope of the executable.

--- src/Evolution/Systems/CurvedScalarWave/Worldtube/PunctureField.cpp
+++ src/Evolution/Systems/CurvedScalarWave/Worldtube/PunctureField.cpp
@@ -27,7 +27,7 @@ void puncture_field(
     const tnsr::I<double, 3>& particle_acceleration, const double bh_mass,
     const std::array<double, 3>& spin, const size_t order) {
   if (order == 0) {
-    puncture_field_0(result, centered_coords, particle_position,
+    puncture_field_kerr_0(result, centered_coords, particle_position,
                      particle_velocity, particle_acceleration, bh_mass, spin);
   } else if (order == 1) {
     puncture_field_1(result, centered_coords, particle_position,
